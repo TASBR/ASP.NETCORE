4b9fa0dc6979fe1f13c8bc717ee9e5f10d574eb1
TASBR <thiagocm100@gmail.com>
2024-09-14 19:24:32 -0300

    Extension Methods and Middlewares

--- DemoVSCode/Program.cs
+++ DemoVSCode/Program.cs
@@ -1,7 +1,12 @@
 //Configuração do Builder
+using DemoVSCode;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Configuração do Pipeline
+builder.AddSerilog();
+
+builder.Services.AddControllersWithViews();
 
 //Midlewares
 
@@ -10,7 +15,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Configuração de comportamentos da App
 var app = builder.Build();
+app.UseLogTempo();
+
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/teste", () =>
+{
+    Thread.Sleep(1500);
+    return "Isso é um teste!";
+});
 
 app.Run();
